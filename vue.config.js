dfcef59c279ea59cc00b0ac03ed021a2c9757388
lify <fly72109@163.com>
2022-11-27 23:11:40 +0800

    modify

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
+/*
+ * @Date: 2022-11-26 15:48:44
+ * @Author: Fancyicookie
+ */
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: './'
 })
